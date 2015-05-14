1d0c016d45529f6bd80a92b6ab9db7b02c52a5b6
sundarrajans <sundarcool14@gmail.com>
2015-05-14 11:43:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='aliens_on_earth',
     version='1.0.0',
-    author='Niyas',
-    author_email='pavilniyas@gmail.com',
+    author='Sundar',
+    author_email='sundarrajan.s66@gmail.com',
     packages=find_packages(exclude=[]),
     scripts=['register'],
-    url='https://github.com/niyaspavil/Aliens-on-Earth',
+    url='https://github.com/sundarrajans/Console-application-for-Aliens-registration',
     description='Aliens-on-Earth is an application to register Aliens details.' ,
     long_description=open('README.md').read(),
     install_requires=[
