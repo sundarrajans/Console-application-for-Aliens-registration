e6646147d1b6bb4943853a1e2ce362054cbac014
sundarrajans <sundarcool14@gmail.com>
2015-05-19 19:26:30 +0530

    Update admin.py

--- Aliens_Registration/admin.py
+++ Aliens_Registration/admin.py
@@ -39,14 +39,3 @@ def get_user_format(ui):
     format =ui.prompt(input)
     return format
 
-def get_formats():
-    """returns list of available fomats/plugins by searching
-    plugin directory"""
-    formats = []
-    format_files = glob.glob("{}/*.py".format(__plugins_dir__))
-    for format_file in format_files:
-        if format_file.endswith("__init__.py"):
-            continue
-        name, ext = splitext(basename(format_file))
-        formats.append(name)
-    return formats
