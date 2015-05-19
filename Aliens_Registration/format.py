b01d97ea9383bd274ddf1a7e8745345c68250620
sundarrajans <sundarcool14@gmail.com>
2015-05-19 19:27:27 +0530

    Update format.py

--- Aliens_Registration/format.py
+++ Aliens_Registration/format.py
@@ -4,3 +4,14 @@ class Format (object):
         raise NotImplementedError()
     def create():
         raise NotImplementedError()
+def get_formats():
+    """returns list of available fomats/plugins by searching
+    plugin directory"""
+    formats = []
+    format_files = glob.glob("{}/*.py".format(__plugins_dir__))
+    for format_file in format_files:
+        if format_file.endswith("__init__.py"):
+            continue
+        name, ext = splitext(basename(format_file))
+        formats.append(name)
+    return formats
